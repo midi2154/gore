05a4f63e157c00da4baa8ab91322c62ce8ba7768
midi2154 <midi2154@gmail.com>
2026-04-15 01:56:33 +0100

    Update main.go

--- goreloaded/main.go
+++ goreloaded/main.go
@@ -8,14 +8,20 @@ import (
 )
 
 func main() {
-	data, err := os.ReadFile("input.txt")
+	if len(os.Args) < 3 {
+		log.Fatal("Usage: go run . input.txt output.txt")
+	}
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+
+	data, err := os.ReadFile(inputFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	output := Process(string(data))
 
-	os.WriteFile("output.txt", []byte(output), 0644)
+	os.WriteFile(outputFile, []byte(output), 0644)
 	fmt.Println("Modification succesful")
 }
 
