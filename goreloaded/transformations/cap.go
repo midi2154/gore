2f9c79ff69eee29a2f2492e3e4dc15a429de0d50
midi2154 <midi2154@gmail.com>
2026-04-15 03:35:55 -0400

    Update cap.go

--- goreloaded/transformations/cap.go
+++ goreloaded/transformations/cap.go
@@ -9,9 +9,9 @@ func Cap(s string) string {
 	for i := 0; i < len(words); i++ {
 		if words[i] == "(cap)" && i > 0 {
 			words[i-1] = strings.ToUpper(words[i-1][:1]) + strings.ToLower(words[i-1][1:])
+				words = append(words[:i], words[i+1:]...)
+
 		}
-		words = append(words[:1], words[i+1:]...)
-		i--
 	}
 	return strings.Join(words, " ")
 }
