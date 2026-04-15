0ebbf3aa38a0254e888decec42e69e7576d98483
midi2154 <midi2154@gmail.com>
2026-04-15 03:51:03 -0400

    Update nword.go

--- goreloaded/transformations/nword.go
+++ goreloaded/transformations/nword.go
@@ -6,21 +6,25 @@ import (
 	"strings"
 )
 
-func main() {
-	fmt.Println("Hello, World!")
-}
-
 func Nword(s string) string {
 	word := strings.Fields(s)
+
 	for i := 0; i < len(word); i++ {
-		if word[i] == "(up," || word[i] == "(low," || word[i] == "(cap," && i+1 < len(word) {
+		if (word[i] == "(up," || word[i] == "(low," || word[i] == "(cap,") && i+1 < len(word) {
+
 			word[i+1] = strings.TrimSuffix(word[i+1], ")")
 			n, err := strconv.Atoi(word[i+1])
 			if err != nil {
 				fmt.Println("Error converting")
 				return ""
 			}
-			for j := i - n; j < i; j++ {
+
+			start := i - n
+			if start < 0 {
+				start = 0
+			}
+
+			for j := start; j < i; j++ {
 				if word[i] == "(up," {
 					word[j] = strings.ToUpper(word[j])
 				}
@@ -28,7 +32,9 @@ func Nword(s string) string {
 					word[j] = strings.ToLower(word[j])
 				}
 				if word[i] == "(cap," {
-					word[j] = strings.ToUpper(word[j][:1]) + strings.ToLower(word[j][1:])
+					if len(word[j]) > 0 {
+						word[j] = strings.ToUpper(string(word[j][0])) + strings.ToLower(word[j][1:])
+					}
 				}
 			}
 			word = append(word[:i], word[i+2:]...)
@@ -37,3 +43,5 @@ func Nword(s string) string {
 	}
 	return strings.Join(word, " ")
 }
+
+
