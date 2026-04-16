68dcf553320843cf84e7f7d4a68c026f2cb1631a
midi2154 <midi2154@gmail.com>
2026-04-16 02:50:33 -0400

    Update low.go

--- goreloaded/transformations/low.go
+++ goreloaded/transformations/low.go
@@ -6,13 +6,14 @@ import (
 
 func Low(s string) string {
 	words := strings.Fields(s)
+
 	for i := 0; i < len(words); i++ {
 		if words[i] == "(low)" && i > 0 {
 			words[i-1] = strings.ToLower(words[i-1])
-					words = append(words[:i], words[i+1:]...)
-
+			words = append(words[:i], words[i+1:]...)
+			i-- // 🔥 critical fix
 		}
-		
 	}
+
 	return strings.Join(words, " ")
 }
