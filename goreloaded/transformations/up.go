fca5aae4d8093ce8b9a6dfb00c80417fd0249b86
midi2154 <midi2154@gmail.com>
2026-04-16 02:46:47 -0400

    Update up.go

--- goreloaded/transformations/up.go
+++ goreloaded/transformations/up.go
@@ -5,14 +5,16 @@ import (
 )
 
 func Up(s string) string {
-
-
 	words := strings.Fields(s)
+
 	for i := 0; i < len(words); i++ {
 		if words[i] == "(up)" && i > 0 {
 			words[i-1] = strings.ToUpper(words[i-1])
 			words = append(words[:i], words[i+1:]...)
+			i-- 
 		}
 	}
+
 	return strings.Join(words, " ")
 }
+
