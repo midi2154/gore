daee1994c1d796dba7e96950087ddec33219cbec
midi2154 <midi2154@gmail.com>
2026-04-16 02:19:30 -0400

    Update punct.go

--- goreloaded/transformations/punct.go
+++ goreloaded/transformations/punct.go
@@ -1,21 +1,13 @@
-package transformations
-
-import (
-	"strings"
-)
-
-var new = strings.NewReplacer(
-	" !!!", "!!!",
-	" ...", "...",
-	" !!", "!!",
-	" ,", ",",
-	" .", ".",
-	" ?", "?",
-	" ;", ";",
-	" :", ";",
-)
-
 func Punct(s string) string {
-	result := new.Replace(s)
-	return result
+	var result strings.Builder
+
+	for i := 0; i < len(s); i++ {
+		if strings.ContainsRune(".,!?;:", rune(s[i])) && i > 0 && s[i-1] == ' ' {
+			result.WriteByte(s[i])
+			continue
+		}
+		result.WriteByte(s[i])
+	}
+
+	return result.String()
 }
