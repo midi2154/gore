4355bdf054cdb40766ebd64ab48e0ea70c410a5a
midi2154 <midi2154@gmail.com>
2026-04-19 18:17:55 -0400

    Update main.go

--- goreloaded/main.go
+++ goreloaded/main.go
@@ -28,13 +28,13 @@ func main() {
 func Process(text string) string {
 	result := transformations.Hex(string(text))
 	result = transformations.Bin(string(result))
+	result = transformations.Nword(string(result))
 	result = transformations.Up(string(result))
 	result = transformations.Low(string(result))
 	result = transformations.Cap(string(result))
 	result = transformations.Articles(string(result))
-	result = transformations.Nword(string(result))
-	result = transformations.Punct(string(result))
 	result = transformations.Quote(string(result))
+	result = transformations.Punct(string(result))
 
 	return result
 }
