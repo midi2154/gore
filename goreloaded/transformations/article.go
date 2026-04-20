6f2ceafa6e2cb3ebfde5c01d1240c48f79fc20b4
midi2154 <midi2154@gmail.com>
2026-04-20 16:14:08 +0100

    Update article.go

--- goreloaded/transformations/article.go
+++ goreloaded/transformations/article.go
@@ -8,11 +8,16 @@ func Articles(text string) string {
 	word := strings.Fields(text)
 
 	for i := 0; i < len(word); i++ {
-		if i+1 < len(word) && strings.ContainsAny(word[i+1][:1], "aeiouhAEIOUH") {
-			if word[i] == "a" {
+		if i+1 < len(word) && len(word[i+1]) > 0 {
+			isVowel := strings.ContainsRune("aeiouhAEIOUH", rune(word[i+1][0]))
+			if word[i] == "a" && isVowel {
 				word[i] = "an"
-			} else if word[i] == "A" {
+			} else if word[i] == "A" && isVowel {
 				word[i] = "An"
+			} else if word[i] == "an" && !isVowel {
+				word[i] = "a"
+			} else if word[i] == "An" && !isVowel {
+				word[i] = "A"
 			}
 		}
 	}
