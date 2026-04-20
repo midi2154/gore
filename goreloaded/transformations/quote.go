6efa550e9250fc70972f069ab5fe97b9ac62cdfa
midi2154 <midi2154@gmail.com>
2026-04-20 15:34:55 +0100

    Update quote.go

--- goreloaded/transformations/quote.go
+++ goreloaded/transformations/quote.go
@@ -4,19 +4,19 @@ import (
 	"regexp"
 )
 
-
 func Quote(s string) string {
-	q1 := regexp.MustCompile(`'\s*(.*?)\s*'`)
-	s = q1.ReplaceAllString(s, " '$1' ")
+	re := regexp.MustCompile(`'\s+(.*?)\s+'`)
+	s = re.ReplaceAllString(s, "'$1'")
 
-	q2 := regexp.MustCompile(`"\s*(.*?)s*"`)
-	s = q2.ReplaceAllString(s, " \"$1\" ")
+	re2 := regexp.MustCompile(`"\s+(.*?)\s+"`)
+	s = re2.ReplaceAllString(s, " \"$1\" ")
 
-	//q3 := regexp.MustCompile((`(\w+)(\s+)`))
-	//s = q3.ReplaceAllString(s, `$1 `)
+	//s = re2.ReplaceAllString(s, `"1$"`)
+	//re3 := regexp.MustCompile(`(\w+)(\s+)`)
+	//s = re3.ReplaceAllString(s, `$1 `)
 
 	text := strings.Fields(s)
 	return strings.Join(text, " ")
-
 }
+
 
