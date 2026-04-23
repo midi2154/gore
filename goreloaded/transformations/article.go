1b74dbe0064c90ce1de853f36014576599831322
midi2154 <midi2154@gmail.com>
2026-04-23 09:10:14 +0100

    Update article.go

--- goreloaded/transformations/article.go
+++ goreloaded/transformations/article.go
@@ -47,3 +47,31 @@ func Articles(s string) string {
 	return strings.Join(word, " ")
 }
 
+
+or 
+
+package transformations
+
+import(
+	"strings"
+)
+func Article(s string) string {
+	f := strings.Fields(s)
+	
+	for i := 0; i < len(f); i++ {
+		text := f[i]
+		word := strings.ContainsAny(string(f[i][0]), "aeiouh'AEIOUH")
+		if text == "an" && word {
+			f[i] = "a"
+		} else if text == "a" && word {
+			f[i] = "an"
+		} else if text == "An" && word {
+			f[i] = "A"
+		} else if text == "A" && word {
+			f[i] = "An"
+		}
+	}
+	return strings.Join(f, " ")
+}
+
+
